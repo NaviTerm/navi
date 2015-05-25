ab8eb3c750e02217424c2ee7c9b3cadebbc8f725
simon-8 <651138009@qq.com>
2015-05-25 17:29:46 +0800

    更新配置
    更新配置文件

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -3,10 +3,10 @@ return array(
 	//'配置项'=>'配置值'
     'LOAD_EXT_CONFIG' => 'config.inc',//引入配置文件
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'dh',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '123456',          // 密码
+    'DB_HOST'               =>  'qdm114262662.my3w.com', // 服务器地址
+    'DB_NAME'               =>  'qdm114262662_db',          // 数据库名
+    'DB_USER'               =>  'qdm114262662',      // 用户名
+    'DB_PWD'                =>  '12345678',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'dh_',    // 数据库表前缀
     // 'TMPL_ENGINE_TYPE'=>'Smarty'
